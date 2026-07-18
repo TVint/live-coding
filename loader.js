b05b957ab870597f5586d6ac9c958c773a650eca
TVint <76667307+TVint@users.noreply.github.com>
2026-07-18 12:27:00 +0200

    Update loader.js

--- loader.js
+++ loader.js
@@ -1,12 +1,10 @@
-// CSS
 const css = document.createElement('link');
 css.rel = 'stylesheet';
-css.href = 'https://raw.githubusercontent.com/TVint/live-coding/main/tv_int.css';
+css.href = 'https://raw.githack.com/TVint/live-coding/main/tv_int.css';
 document.head.appendChild(css);
 
-// JS
 const js = document.createElement('script');
-js.src = 'https://raw.githubusercontent.com/TVint/live-coding/main/tv_int.js';
+js.src = 'https://raw.githack.com/TVint/live-coding/main/tv_int.js';
 document.head.appendChild(js);
 
-console.log("🛠️ Loader eseguito: librerie in fase di aggancio...");
+console.log("🚀 Loader eseguito con Githack: librerie in fase di aggancio...");
